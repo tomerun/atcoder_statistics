daf0128cb8180fe9f4efeefdea4cebed4a06baaa
tomerun <tomerunmail@gmail.com>
2016-11-30 17:48:44 +0900

    use grid search

--- src/guess_point.py
+++ src/guess_point.py
@@ -1,23 +1,28 @@
 # coding: utf-8
 
 import itertools
-from sklearn.linear_model import RidgeCV
-from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import RidgeCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVR
 import numpy as np
 import pandas as pd
 import database
 from data_structure import *
 
 def normalize(value):
+	# return value / 10000.0
 	return np.log(value / 10000.0)
 
 def denormalize(value):
+	# return value * 10000.0
 	return np.power(np.e, value) * 10000.0
 
 
 def main():
-	train_data = pd.read_csv('train.csv').drop('source', 1)
+	train_data = pd.read_csv('train.csv')
+	train_pid = train_data['source']
+	train_data = train_data.drop('source', 1)
 	test_data = pd.read_csv('test.csv')
 	test_pid = test_data['source']
 	test_data = test_data.drop('source', 1)
@@ -25,7 +30,7 @@ def main():
 	# extract users having many features in test_data
 	col_sum = test_data.sum(0)
 	user_count = col_sum.rolling(2).sum()[1:-1:2].astype(int)
-	effective_users = [l[0:-2] for l, v in user_count.iteritems() if v >= 20]
+	effective_users = [l[0:-2] for l, v in user_count.iteritems() if v >= 100]
 	effective_cols = [name for user in effective_users for name in [user + '_T', user + '_F']]
 	print('test_user_count', len(effective_users))
 
@@ -33,38 +38,48 @@ def main():
 	train_data = train_data[effective_cols]
 	test_data = test_data[effective_cols]
 
-	mask = np.random.rand(len(train_data)) <= 1.0
-	train_selected = train_data[mask]
-	train_expect = expect[mask]
-	verify_selected = train_data[~mask]
-	verify_expect = expect[~mask]	
+	mask = np.random.rand(len(train_data)) >= 1.0
+	# is_cf_2016 = lambda x : 2128 <= x and x <= 2137
+	# mask = train_pid.apply(is_cf_2016)
+	train_selected = train_data[~mask]
+	train_expect = expect[~mask]
+	verify_selected = train_data[mask]
+	verify_expect = expect[mask]
 
-	# regressor = RidgeCV(alphas = (0.1, 0.3, 0.5, 1, 3, 5, 10, 20), cv = 3)
-	# regressor.fit(train_selected, train_expect)
-	# print(list(regressor.coef_))
-	# print(regressor.intercept_)
-	# print(regressor.alpha_)
+	# tuned_parameters = [{'alphas': [[0.1], [0.3], [1], [3], [10], [30], [100], [300]],
+	#                      },
+	#                     ]
+	# regressor = GridSearchCV(RidgeCV(), tuned_parameters, cv=4)
 
-	# regressor = SVR(C=5, epsilon=0.1)
-	# regressor.fit(train_selected, train_expect)
+	tuned_parameters = [{'kernel': ['rbf'],
+	                     'gamma': [1e-2, 3e-2, 1e-3, 3e-3, 1e-4],
+	                     'C': [1, 3, 10, 30, 100, 300],
+	                     'epsilon': [0, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1],
+	                     },
+	                    ]
+	regressor = GridSearchCV(SVR(), tuned_parameters, cv=4)
 
-	regressor = RandomForestRegressor(n_estimators=1000)
+	# tuned_parameters = [{'n_estimators': [27, 81, 243],
+	#                      'max_features': ['auto', 'sqrt', 'log2'],
+	#                      'min_samples_split': [2, 3, 4],
+	#                      # 'max_leaf_nodes': [None, 2, 4, 8, 16, 32],
+	#                      },
+	#                     ]
+	# regressor = GridSearchCV(RandomForestRegressor(), tuned_parameters, cv=4)
+
 	regressor.fit(train_selected, train_expect)
-	print(regressor.estimators_)
-	print(regressor.feature_importances_)
-	print(regressor.n_features_)
-	print(regressor.n_outputs_)
+	print(regressor.best_params_)
+	print(regressor.best_score_)
 
 	train_result = regressor.predict(train_selected)
 	for e, r in zip(denormalize(train_expect), denormalize(train_result)):
-		print(e, r)
+		print(int(round(e / 100)), int(round(r / 100)))
 	print()
 
 	# verify_result = regressor.predict(verify_selected)
-	# for e, r in zip(verify_expect, verify_result):
-	# 	print(e, r)
+	# for e, r in zip(denormalize(verify_expect), denormalize(verify_result)):
+	# 	print(int(round(e / 100)), int(round(r / 100)))
 	# print()
-
 
 	db = database.get_connection()
 	with closing(db) as con:
@@ -74,11 +89,7 @@ def main():
 	test_result = denormalize(regressor.predict(test_data))
 	result = [(pid_to_task[pid].contest_id, pid_to_task[pid].symbol, pid, r) for pid, r in zip(test_pid, test_result)]
 	for res in sorted(result):
-		print(res[2], res[0], res[1], res[3])
-
-	# for pid, r in zip(test_pid, test_result):
-	# 	task = pid_to_task[pid]
-	# 	print(pid, task.contest_id, task.symbol, r)
+		print(res[2], res[0], res[1], int(round(res[3] / 100)))
 	print()
 
 if __name__ == '__main__':
